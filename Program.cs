421805f5154c18f0292968835e91cf74f7ab074d
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-16 18:57:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Requêtes_Faciles_3__.NET_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nombres supérieurs à 2, qui sont des multiples de 2, inférieurs à 20, différents de 8
+
+            List<int> Entiers = new List<int> { 2, 3, 5, 1, 58, 46, 31, 14, 82, 0, 6 };
+
+            var query = from x
+                        in Entiers
+                        where x > 2
+                        where x % 2 == 0
+                        where x < 20
+                        select x;
+
+            foreach (var x in query)
+            {
+                Console.WriteLine(x);
+            }
+
+
+        }
+    }
+}
